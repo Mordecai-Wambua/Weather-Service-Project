7cfe05f3b91e7b0ffbd9a34549d30a42026609c2
morde <mordecaingumbau@gmail.com>
2024-07-03 20:40:35 +0300

    Feature: enables saving of user form data

--- v3/app.py
+++ v3/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
+import os
 import requests
 from sys import argv
 
@@ -6,12 +8,34 @@ app = Flask(__name__)
 
 API_KEY = argv[1]
 DEFAULT_CITY = 'Nairobi'
+FORM_DATA_FILE = 'form_data.json'
 
 @app.route('/')
 def dashboard():
     city = request.args.get('city', DEFAULT_CITY)
     weather_data = get_weather_data(city)
     return render_template('index.html', weather_data=weather_data, city=city)
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    form_data = {
+        'name': request.form['name'],
+        'number': request.form['number'],
+        'email': request.form['email'],
+        'message': request.form['message']
+    }
+    
+    if os.path.exists(FORM_DATA_FILE):
+        with open(FORM_DATA_FILE, 'r+') as file:
+            data = json.load(file)
+            data.append(form_data)
+            file.seek(0)
+            json.dump(data, file, indent=4)
+    else:
+        with open(FORM_DATA_FILE, 'w') as file:
+            json.dump([form_data], file, indent=4)
+
+    return redirect(url_for('dashboard'))
 
 def get_weather_data(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
@@ -53,7 +77,7 @@ def get_forecast_data(lat, lon):
     data = response.json()
     forecast = []
     if response.status_code == 200:
-        for i in range(0, len(data['list']), 8):  # OpenWeatherMap provides data every 3 hours, so 8 periods per day
+        for i in range(0, len(data['list']), 8):
             day_data = data['list'][i]
             forecast.append({
                 'date': day_data['dt_txt'].split(' ')[0],
